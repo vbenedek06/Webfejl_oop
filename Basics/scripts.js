993e5d87c972841a9cab3a74ec874a701262b9a9
Virag Benedek <virag.benedek@BOLYAI.LOCAL>
2024-11-29 12:48:29 +0100

    gomszab player

---
+++ Basics/scripts.js
@@ -0,0 +1,25 @@
+function Player(nickname) {
+    this.nickname = nickname
+    this.playedMatch = 0
+}
+
+Player.prototype.play = function () {
+    this.playedMatch++
+    console.log(this.nickname, this.playedMatch)
+}
+
+Player.prototype.getTierLevel = function () {
+    if (this.playedMatch >= 0 && this.playedMatch <= 3) {
+        return 'A'
+
+    } else if (this.playedMatch >= 4 && this.playedMatch <= 6) {
+        return 'B'
+    } else {
+        return 'C'
+    }
+
+}
+const gomszab = new Player('gomszab')
+console.log(gomszab);
+gomszab.play()
+console.log(gomszab.getTierLevel())
