d0eb23b3b4f0c700a3b02570a602e501ad9b005c
Virag Benedek <virag.benedek@BOLYAI.LOCAL>
2024-11-29 12:58:30 +0100

    orai munka folyt

--- Basics/scripts.js
+++ Basics/scripts.js
@@ -1,23 +1,46 @@
-function Player(nickname) {
-    this.nickname = nickname
-    this.playedMatch = 0
-}
+// function Player(nickname) {
+//     this.nickname = nickname
+//     this.playedMatch = 0
+// }
 
-Player.prototype.play = function () {
-    this.playedMatch++
-    console.log(this.nickname, this.playedMatch)
-}
+// Player.prototype.play = function () {
+//     this.playedMatch++
+//     console.log(this.nickname, this.playedMatch)
+// }
 
-Player.prototype.getTierLevel = function () {
-    if (this.playedMatch >= 0 && this.playedMatch <= 3) {
-        return 'A'
+// Player.prototype.getTierLevel = function () {
+//     if (this.playedMatch >= 0 && this.playedMatch <= 3) {
+//         return 'A'
 
-    } else if (this.playedMatch >= 4 && this.playedMatch <= 6) {
-        return 'B'
-    } else {
-        return 'C'
+//     } else if (this.playedMatch >= 4 && this.playedMatch <= 6) {
+//         return 'B'
+//     } else {
+//         return 'C'
+//     }
+
+// }
+
+
+
+class Player {
+    constructor(nickname) {
+        this.nickname = nickname
+        this.playedMatch = 0
+
     }
-
+    play() {
+        this.playedMatch++
+        console.log(this.nickname, this.playedMatch)
+    }
+    getTierLevel() {
+        if (this.playedMatch >= 0 && this.playedMatch <= 3) {
+            return 'A'
+        } else if (this.playedMatch >= 4 && this.playedMatch <= 6) {
+            return 'B'
+        } else {
+            return 'C'
+        }
+    }
 }
 const gomszab = new Player('gomszab')
 console.log(gomszab);
