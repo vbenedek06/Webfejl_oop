7d862638c8e8b7851f4f05ffaf4586f0deb684ca
Virag Benedek <virag.benedek@bolyai.local>
2024-12-06 10:43:39 +0100

    Santa

---
+++ Santa/classes.js
@@ -0,0 +1,8 @@
+
+class Factory{
+ // TODO 1, 2, 3, 4, 9, 10
+}
+
+class Companion{
+ // TODO 5
+}
---
+++ Santa/main.js
@@ -0,0 +1,64 @@
+/**
+ * @type {{
+ *   firstName: string;
+ *   lastName: string;
+ *   products: string[];
+ *   area: string;
+ * }[]}
+ */
+const companionList = [
+    {
+        firstName: 'Géza',
+        lastName: 'Kiss',
+        area: 'plastic',
+        products: ['kisauto', 'barbibaba']
+    },
+    {
+        firstName: 'Ferenc',
+        lastName: 'Tóth',
+        area: 'paper',
+        products: ['könyv']
+    },
+    {
+        firstName: 'Gábor',
+        lastName: 'Nagy',
+        area: 'plastic',
+        products: ['zokni']
+    },
+]
+const factory = new Factory();
+
+document.getElementById('companion').addEventListener('submit',function(e){
+    e.preventDefault();
+    const form =  e.currentTarget
+    addCompanion(form, factory);
+});
+
+document.getElementById('product').addEventListener('submit',function(e){
+    e.preventDefault();
+    const form = e.currentTarget;
+    addProductForm(form, factory)
+});
+
+/**
+ * table render
+ */
+function initTable(){
+
+    // TODO 6
+}
+
+
+initTable()
+
+/**
+ * 
+ * eventlistener callback for the button click of companion
+ * 
+ * @param {EventTarget} e 
+ */
+function checkEventListener(e){
+    const row = e.currentTarget.parentElement.parentElement;
+    const companionId = row.id;
+    // TODO 10
+}
