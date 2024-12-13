7c8db84a2443aa4e40cc4a9cf6861e89717fb8d0
Virag Benedek <virag.benedek@BOLYAI.LOCAL>
2024-12-13 11:10:42 +0100

    added

---
+++ Table/scripts.js
@@ -0,0 +1,21 @@
+const array = [
+    {
+        firstname1: 'Géza',
+        firstname2: 'Ferenc',
+        lastname: 'Kocsis',
+    },
+    {
+        firstname1: 'Mária',
+        firstname2: 'Júlia',
+        lastname: 'Horváth',
+    },
+    {
+        firstname1: 'Ferenc',
+        lastname: 'Balogh',
+    },
+    {
+        firstname1: 'Gábor',
+        firstname2: 'Attila',
+        lastname: 'Horváth'
+    },
+]
